d5aca3d4257c67e5ddaf6fe1f607c0a80f563560
‘josh <kingorijosphat@yahoo.com>
2022-10-12 14:41:28 -0400

    Git Project

---
+++ jos-centos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+sudo yum update -y 
+sudo yum install git -y 
+sudo yum install wget -y 
+sudo yum install net-tools -y 
+
+sudo nproc -y 
+
+sudo top -y 
+
+sudo lsblk - y 
+
+git init -y 
