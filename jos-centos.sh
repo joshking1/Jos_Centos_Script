9f4ada1175e3a2c01b65641f23de52b4e677e932
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-10-13 08:19:39 -0400

    Update jos-centos.sh

--- jos-centos.sh
+++ jos-centos.sh
@@ -8,6 +8,19 @@ sudo yum install docker -y
 
 sudo useradd Rose 
 
+sudo yum install docker-compose -y 
+
+sudo yum install 
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip 
+
+sudo ./aws/install
+
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+aws --version
 
 sudo nproc -y 
 
